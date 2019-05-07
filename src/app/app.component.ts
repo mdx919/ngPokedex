c091fd2e3703bb3f373dcb77830dc069069b5025
shlok297 <shlokipatel@gmail.com>
2019-05-07 10:57:52 -0400

    added search params and functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,59 @@ import { Pokemons } from '../pokedex';
 export class AppComponent {
   title = 'Pokedex';
 
-  pokemons = Pokemons;
+  pokemons;
+
+  startSearch(searchParam:any, dropdown: any) {
+    
+    // console.log(searchParam);
+    // console.log(dropdown);
+    let result = [];
+
+    if (searchParam.length > 0 && dropdown !== 0) {
+      Pokemons.map((item) => { 
+        if (dropdown === 'name') {
+          if(item.name === searchParam) {
+            result.push(item);
+          }
+        } else if (dropdown === 'attacks' && item.attacks) {
+          item.attacks.map((attack) => {
+            if(attack.name === searchParam) {
+              result.push(item);
+            }
+          })
+        } else if (dropdown === 'types' && item.types) {
+          item.types.map((type) => {
+            if(type === searchParam) {
+              result.push(item);
+            }
+          })
+        } else if (dropdown === 'weaknesses' && item.weaknesses) {
+          item.weaknesses.map((weakness) => {
+            if(weakness.type === searchParam) {
+              result.push(item);
+            }
+          })
+        } else if (dropdown === 'rarity' && item.rarity) {
+            if(item.rarity === searchParam) {
+              result.push(item);
+            }
+        } else if (dropdown === 'evolvesFrom' && item.evolvesFrom) {
+            if(item.evolvesFrom === searchParam) {
+              result.push(item);
+            }
+          } else if (dropdown === 'lvl' && item.level) {
+            if(item.level >= searchParam) {
+              result.push(item);
+            }
+          } else if (dropdown === 'hp' && item.hp) {
+            if(item.hp >= searchParam) {
+              result.push(item);
+            }
+          }
+      })
+    }
+    this.pokemons = result;
+    console.log(this.pokemons);
+    
+  }
 }

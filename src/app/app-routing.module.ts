679da8c886fb3574c507ab58a915aaf0d4ab8f29
shlok297 <shlokipatel@gmail.com>
2019-05-07 16:03:11 -0400

    trying to add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component'; 
 import { AppComponent } from './app.component'; 
 
 const routes: Routes = [
-  { path: 'pokemons', component: AppComponent }
+  { path: 'pokemon/:id', component: DashboardComponent },
+  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
